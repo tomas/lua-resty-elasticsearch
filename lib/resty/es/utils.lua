cd8578c2a74af04fd61c21716a2566795beb7576
Tomás Pollak <tomaspollak@gmail.com>
2017-10-07 00:09:03 -0300

    Got a working version

--- lib/resty/es/utils.lua
+++ lib/resty/es/utils.lua
@@ -1,17 +1,17 @@
 local cjson = require "cjson"
 
-local t_insert = table.insert 
+local t_insert = table.insert
 local t_concat = table.concat
 local str_format = string.format
 local select = select
 
 
 local _M = {
-    _VERSION = '0.01'
+    _VERSION = '0.02'
 }
 
 
-function _M.make_path(...) 
+function _M.make_path(...)
     local t = {}
     local num_args = select('#', ...)
     for i = 1, num_args do
@@ -20,7 +20,7 @@ function _M.make_path(...)
             t_insert(t, arg)
         end
     end
-    return '/' .. t_concat(t, '/') 
+    return '/' .. t_concat(t, '/')
 end
 
 
@@ -39,11 +39,30 @@ function _M.get_err_str(self, http_response_code, http_response_data)
 end
 
 
-function _M.deal_params(s_params, query_params_list)
+-- usage: basic_params, query_params = deal_params(params, { 'foo', 'bar' })
+function _M.deal_params(s_params, basic_params_list)
     if s_params == nil then
         return {}, {}
     end
+
+    local basic = {}
+    local query = {}
+
+    for _, key in ipairs(basic_params_list or {} ) do
+      basic[key] = 1
+    end
+
+    for key, val in pairs(s_params) do
+      if basic[key] then
+        basic[key] = val
+      else
+        query[key] = val
+      end
+    end
+
+    return basic, query
 end
+
 
 
 return _M
